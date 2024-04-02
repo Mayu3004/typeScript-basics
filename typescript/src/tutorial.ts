5ce3151cba36e557b217e3bf834ef7008fd8b2ec
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-02 16:26:27 +0530

    Started the function module completed till 1:19:43

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -97,10 +97,98 @@ let temperatures: number[] = [12.3,90.4,0,100];
 let colors: string[] = ['red','blue','green'];
 let mixedArray: (string|number)[] = ['red','blue',10.20];
 
+//OBJECTS AND FUNCTIONS
 
+let car: {brand:string,year:number} = {
+    brand: 'toyota',
+    year: 2024
+}
+car.brand = 'ford';
 
+let car1: {brand:string,year:number} = {
+    brand: 'audi',
+    year: 2023
+}
 
+let book = {title:'book',cost:20};
+let pen = {title:'pen',cost:10};
+let notebook = {title:'notebook'};
+let items:{readonly title: string,cost?:number}[]= [pen,book,notebook]; //? optional property  readonly=>can read cannot change value
 
+//CHALLENGES
+// 1.Create an object bike of type { brand: string, year: number } and assign it some values. Then, try to assign a string to the year property.
+// 2.Create an object laptop of type { brand: string, year: number } and try to assign an object with missing year property to it.
+// 3.Create an array products of type { title: string, price?: number }[] and assign it some values. Then, try to add an object with a price property of type string to it.
 
+let bike: {brand:string,cost:number} = {
+    brand:'Bullet',
+     cost:150000
+    }
 
+let laptop: {brand:string,year:number} = {
+    brand:'Dell',
+     year:2023
+    }
 
+let productArray: {title:string,price?:number}[]=[
+    {
+        title:'Laptop',
+        price:150000
+    },
+    {
+        title:"Bike"
+    }
+];
+let product1:{title:string,price?:number} = {title:'shoes',price:3500};
+productArray.push(product1);
+
+//FUNCTIONS
+
+// function sayHi(name){
+        
+// } //if we dont give the type it gives error so there are three ways to define it.
+//1.any
+//2.config file changes
+//type
+
+function sayHi(name:string){
+    console.log(`Hello,Gud mrng ${name.toUpperCase()}!!!`);
+}
+sayHi('Mayur')
+
+function calculateDiscount(price:number):number {
+    const hasDiscount = true;
+    if(hasDiscount){
+        // return 'Discount applied'; // gives error becoz this function can return only number
+    }
+    return price*0.9;
+
+}
+
+const ans = calculateDiscount(200);
+
+function addThree(number: any){
+    let anotherNumber: number = 3;
+    return number+anotherNumber;
+}
+const res = addThree(10);
+console.log(res);
+
+//CHALLENGE
+// 1.Create a new array of names.
+// 2.Write a new function that checks if a name is in your array. This function should take a name as a parameter and return a boolean.
+// 3.Use this function to check if various names are in your array and log the results.
+
+let nameStudent:string[] = ['Mayur','Amit','Yuviraj','Arpita'];
+
+function isNamePresent(nameStudent:string[]):boolean{
+
+    for(let i = 0;i<nameStudent.length;i++){
+        if(nameStudent[i] === 'Mayur'){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(isNamePresent(nameStudent))
