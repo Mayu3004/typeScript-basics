756efa638dfb07c06384e74d9b19d061956ec4fb
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-04 15:49:08 +0530

    completed the challenge and the context part till 8:16

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,9 +1,13 @@
 // import Component from "./starter/03-state";
-import Component from "./starter/04-events";
+// import Component from "./starter/04-events";
+// import Component from "./starter/05-challenge";
+// import Component from "./starter/06-context";
+import Component from "./starter/07-reducers";
 
 function App() {
   return (
     <main>
+
       <Component/>
     </main>
   );
--- 02-typescript-react-essentials/src/starter/06-context/index.tsx
+++ 02-typescript-react-essentials/src/starter/06-context/index.tsx
@@ -1,9 +1,28 @@
+import { useTheme, ThemeProvider } from './context';
+
+function ParentComponent() {
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  );
+  return <Component />;
+}
 function Component() {
+  const context = useTheme();
+  console.log(context);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Context API</h2>
+      <h2>Any component</h2>
+      <button onClick={()=>{
+          if (context.theme ==='dark'){
+            context.setTheme('system');
+            return;
+          }context.setTheme('dark');
+        }}
+        className='btn btn-center'>
+        Toggle Theme</button>
     </div>
   );
 }
-export default Component;
+export default ParentComponent;
