7f18e6382c327b17e545dedd2cb625e5e6064cdb
mayur.limbhore-coditas <mayur.limbhore@coditas.com>
2024-04-04 13:52:42 +0530

    Organised folder 1

--- 01 typescript/typescript/src/main.ts
+++ 01 typescript/typescript/src/main.ts
@@ -9,7 +9,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="/tasks" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="#tasks" target="_blank">
+    <a href="/tasks" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
